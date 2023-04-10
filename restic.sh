9f2427a986d9b98a12bbaa96ed7c11513d00b2b1
hcuk94 <me@hc.me.uk>
2023-04-10 21:13:37 +0100

    add emailer of log file

--- restic.sh
+++ restic.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Variables
+MAIL_FROM=restic@example.com
+MAIL_TO=restic@example.com
+SMTP_SERVER=server:587
+
 # Timestamp Function
 timestamp() {
 date "+%b %d %Y %T %Z"
@@ -8,7 +13,7 @@ date "+%b %d %Y %T %Z"
 # Log Start
 printf "\n\n"
 echo "-------------------------------------------------------------------------------" | tee -a restic.log
-echo "$(timestamp): restic.sh started" | tee -a restic.log
+echo -e "$(timestamp): restic.sh started\n" | tee -a restic.log
 
 # Run Backups
 restic backup /home /etc /root /var /usr/local/bin /usr/local/sbin /opt
@@ -25,4 +30,10 @@ restic check | tee -a restic.log
 # Log End
 printf "\n\n"
 echo "-------------------------------------------------------------------------------" | tee -a restic.log
-echo "$(timestamp): restic.sh finished" | tee -a restic.log
+echo -e "$(timestamp): restic.sh finished\n" | tee -a restic.log
+
+# Mail Results and clear log/mail files
+echo -e "Subject: Restic backup report for $HOSTNAME\n\n" >> mailcontent.txt
+cat restic.log >> mailcontent.txt
+curl smtp://$SMTP_SERVER --mail-from $MAIL_FROM --mail-rcpt $MAIL_TO -T mailcontent.txt
+rm restic.log mailcontent.txt
